3bad45db70b787ab2bd358ca0572b2f995f8a169
cuiqs <cuiqs@126.com>
2019-10-16 18:04:02 +0800

    实现对蒸汽压和液化气C5两个值预测

--- online.py
+++ online.py
@@ -53,28 +53,33 @@ def predict():
         val=val*50+50
         print(val)
 
-
-def write_to_xml(fore_value, filename):
+#把预测值写入xml文件
+def write_to_xml(val_dict, filename):
     doc = Document()
     root = doc.createElement("prediction")
     doc.appendChild(root)
 
     tnode = doc.createElement("time")
     cur_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    txnode = doc.createTextNode(cur_time)
-    tnode.appendChild(txnode)
 
-    valnode = doc.createElement("predict value")
-    val = doc.createTextNode(str(fore_value))
-    valnode.appendChild(val)
+    tnode.setAttribute("time",cur_time)
+
+
+    for item in val_dict.keys():
+        snode=doc.createElement(item)
+        stnode=doc.createTextNode(val_dict[str(item)])
+        snode.appendChild(stnode)
+        tnode.appendChild(snode)
+
     root.appendChild(tnode)
-    root.appendChild(valnode)
+
 
     with open(filename, 'w') as f:
         f.write(doc.toprettyxml(indent='\t'))
 
         
-def predict2(sample):
+def predict2(parameters,cur_pvs,line,scale,base):
+    """
     if sample=="lpgc5":
         cur_pvs=np.load("lpgc5_pvs.npy")
         paras=train.get_parameters("lpgc5_trained_parameters.npz")
@@ -87,21 +92,42 @@ def predict2(sample):
         line=get_a_line_fromxml("book.xml")
         base=50
         scale=50
-        
+     """
     cur_pvs=np.delete(cur_pvs,-1,axis=0)
     cur_pvs=np.insert(cur_pvs,0,line,axis=0)
     cur_pvs_trans=cur_pvs.reshape(-1,1)
-    pre_val=train.forward_propagation(cur_pvs_trans,paras)
+    pre_val=train.forward_propagation(cur_pvs_trans,parameters)
     sess=tf.Session()
     val=sess.run(pre_val)
+
        
     val=val*scale+base
-    write_to_xml(val,"predict.xml")
+    return val
+  #  write_to_xml(val,"predict.xml")
 
 
 if __name__=="__main__":
 
+    val_dict={}
     #print(len(vapor_press_pvs))
-    predict2("lpgc5")
+    pa_vapor=train.get_parameters("vapor_press_trained_parameters.npz")
+    cur_pvs=np.load("pvs.npy")
+    line=get_a_line_fromxml("book.xml")
+
+    scale_vapor=gettrain_x.sample_span["stagaspresh"]-gettrain_x.sample_span["stagaspresl"]
+    base_vapor=gettrain_x.sample_span["stagaspresl"]
+    vapor_predict_val=predict2(pa_vapor,cur_pvs,line,scale_vapor,base_vapor);
+    val_dict["vapor_press"]="{:.3f}".format(vapor_predict_val[0][0])
+
+    pa_lpg=train.get_parameters("lpgc51801-1906_head2_trained_parameters.npz")
+    scale_lpgc5 = gettrain_x.sample_span["lpgc5h"]-gettrain_x.sample_span["lpgc5l"]
+    base_lpgc5 = gettrain_x.sample_span["lpgc5l"]
+    lpg_predict_value=predict2(pa_lpg,cur_pvs,line,scale_lpgc5,base_lpgc5);
+    if lpg_predict_value[0][0]<0:
+        lpg_predict_value[0][0]=0.000
+
+    val_dict["lpgc5"]="{:.3f}".format(lpg_predict_value[0][0])
+
+    write_to_xml(val_dict,"predict.xml")
 
 
